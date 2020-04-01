2e6505bffb0c8170b8e476c816348f83c04686c1
eric-austin <eric.james.austin@gmail.com>
2020-04-01 13:32:21 -0600

    basic rule building working

--- src/Rule.java
+++ src/Rule.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
 
 public class Rule {
+	//instance variables
+	ArrayList<String> antecedent = null;
+	String consequent = null;
 
+	public Rule(ArrayList<String> ant, String con) {
+		antecedent = new ArrayList<String>(ant);
+		consequent = new String(con);
+	}
+	
+	@Override
+	public String toString() {
+		return (antecedent.toString() + " -> " + consequent);
+	}
 }
--- src/RuleBuilder.java
+++ src/RuleBuilder.java
@@ -1,8 +1,9 @@
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import weka.classifiers.trees.J48;
+import weka.core.Attribute;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
@@ -32,18 +33,118 @@ public class RuleBuilder {
 			//set attribute to target class based on user given input
 			data.setClassIndex(target);
 			//set up options for tree
-			String[] options = weka.core.Utils.splitOptions("-M 100");
+			String[] options = weka.core.Utils.splitOptions("");
 			tree.setOptions(options);
+			//use J48 tree and given options to build decision tree
 			tree.buildClassifier(data);
-			String treeVisual = tree.toString();
-			System.out.println(treeVisual);
-			//System.out.println("Size of string: " + treeVisual.getBytes().length/(Math.pow(1024, 2)) + "MB");
-			//FileWriter writer = new FileWriter("output.txt");
-			//System.out.println("tree written to file");
+			
+			System.out.println("Tree has " + tree.measureNumRules() + " rules.");
+			//get string representation of tree
+			String treeString =  tree.toString();
+			//break into the lines and remove header/footer to get only lines representing nodes
+			String[] lines = treeString.split("\n");
+			lines = getNodes(lines);
+			
+			//build rules from lines
+			Attribute classAttribute = data.classAttribute();
+			String classAttString = classAttribute.toString();
+			//need to grab name of class attribute/feature
+			classAttString = (classAttString.split(" "))[1];
+			ArrayList<Rule> ruleSet = buildRuleSet(lines, classAttString);
+			
+			System.out.println(treeString);
+			System.out.println("We found " + ruleSet.size() + " rules.");
+			for (Rule r : ruleSet) {
+				System.out.println(r.toString());
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+	}
+	
+	public static String[] getNodes(String[] lines) {
+		String[] nodes = Arrays.copyOfRange(lines, 3, lines.length - 4);
+		return nodes;
+	}
+	
+	public static ArrayList<Rule> buildRuleSet(String[] lines, String classFeature) {
+		//set up new arraylist representing ruleset
+		ArrayList<Rule> ruleset = new ArrayList<Rule>();
+		//split up the node lines for parsing
+		String[][] splitNodes = new String[lines.length][];
+		for (int i = 0; i < lines.length; i++) {
+			splitNodes[i] = lines[i].split(" ");
+		}
+		//set up local vars to hold temp rule antecedent as we build it
+		int currentDepth = 0;
+		ArrayList<String> tempAnt = new ArrayList<>();
+		//iterate through lines of tree
+		for (String line : lines) {
+			//if line represents a leaf we need to build a rule
+			if (isLeaf(line)) {
+				//check the depth of the leaf
+				int leafDepth = getDepth(line);
+				//if leaf depth is equal to current depth, we can add the next
+				//feature value and the leaf value
+				if (leafDepth == currentDepth) {
+					//break up the line to get the last antecedent value and class value
+					String feature = line.split(":")[0];
+					String target = line.split(":")[1];
+					//break up the strings to parse for the values we want
+					String[] fSplit = feature.split(" ");
+					String[] tSplit = target.split(" ");
+					//get the feature and value and append to the end of antecedent
+					tempAnt.add(fSplit[fSplit.length - 3] + " = " + fSplit[fSplit.length - 1]);
+					//create new rule and add to ruleset
+					ruleset.add(new Rule(tempAnt, classFeature + " = " + tSplit[1]));
+					//remove last antecedent value since this cannot be used twice
+					tempAnt.remove(currentDepth);
+				} else {
+					//if leaf depth is <= to current depth, need to go back up tree
+					//ie deleting parts of the antecedent until we are right below leaf
+					while(currentDepth > leafDepth) {
+						currentDepth--;
+						tempAnt.remove(currentDepth);
+					}
+					//break up the line to get the last antecedent value and class value
+					String feature = line.split(":")[0];
+					String target = line.split(":")[1];
+					//break up the strings to parse for the values we want
+					String[] fSplit = feature.split(" ");
+					String[] tSplit = target.split(" ");
+					//get the feature and value and append to the end of antecedent
+					tempAnt.add(fSplit[fSplit.length - 3] + " = " + fSplit[fSplit.length - 1]);
+					//create new rule and add to ruleset
+					ruleset.add(new Rule(tempAnt, classFeature + " = " + tSplit[1]));
+					//remove last antecedent value since this cannot be used twice
+					tempAnt.remove(currentDepth);
+				}
+			} else {
+				//if next path is not to a leaf then we add to antecedent and increment depth
+				String[] split = line.split(" ");
+				tempAnt.add(split[split.length - 3] + " = " + split[split.length - 1]);
+				currentDepth++;
+			}
+		}
+		
+		return ruleset;
+	}
+	
+	public static int getDepth(String line) {
+		int depth = 0;
+		String[] strings = line.split(" ");
+		for (String s : strings) {
+			if (s.contains("|")) {
+				depth++;
+			}
+		}
+		return depth;
+	}
+
+	public static boolean isLeaf(String line) {
+		return (line.contains(":"));
 	}
 
 }
